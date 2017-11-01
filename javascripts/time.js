f6c861250cae164d8f50d8b82d72111e3c34aeef
Alex Sanchez <alesanchez728@gmail.com>
2017-10-31 21:10:33 -0500

    added skeleton functions and comments for each

---
+++ javascripts/time.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const getCurrentTime = () => {
+	// calls moment function ...
+		// ...returns the current time
+
+};
+
+const getSelectedTime = () => {
+	// uses dropDown time picker ...
+		// .. returns selected time
+};
